9ad43ec2b7bd65ca76219b421241c4153d3fcb2f
molsrg <molsrg.35@gmail.com>
2023-08-08 16:45:39 +0300

    Improved authorization
    Almost completed the authorization form. There are shortcomings on <hr/>"or". Now we need to write the logic for sending data to the API.

--- client/src/components/Auth/AuthorizationClient.vue
+++ client/src/components/Auth/AuthorizationClient.vue
@@ -1,9 +1,54 @@
 <template>
-  <div class="form"></div>
+  <div class="section">
+    <div class="section__item">
+      <div class="section__logo">
+        <img class="logo" src="../../assets/images/auth/logo.svg" alt="logo">
+        <h3 class="section__text">Task Unity Tech</h3>
+      </div>
+    </div>
+
+    <div class="section__item">
+      <div class="section__auth">
+        <button class="section__title" @click="toFalse">Регистрация</button>
+        <button class="section__subtitle" @click="toFalse">Вход</button>
+      </div>
+      
+    </div>
+    <div class="section__item">
+      <form action="" method="post">
+        <input class="section__input" type="text" placeholder="Имя пользователя" name="name">
+        <input class="section__input" type="email" placeholder="E-mail" name="email">
+        <input class="section__input" type="password" placeholder="Пароль" name="password">
+        <input class="section__input" type="password" placeholder="Подтверждение пароля" name="confirm-password">
+      </form>
+    </div>
+
+    <div class="section__item">
+      <button type="submit" class="section__button">Зарегистрироваться</button>
+      <hr>
+      <div>или</div>
+      <div class="section__bt-container">
+        <a href="#"><img src="../../assets/images/auth/google.svg"></a>
+        <a href="#"><img src="../../assets/images/auth/github.svg"></a>
+        <a href="#"> <img src="../../assets/images/auth/facebook.svg"></a>
+      </div>
+
+    </div>
+  </div>
+
+  <!-- <img :src="flag == true ? require('../../assets/images/auth/ile.png') : require('../../assets/images/auth/ile2.png')"> -->
+
 </template>
 
 <script>
 export default {
+  data(){
+    return { 
 
+    }
+  }, 
+  methods: {
+    
+  }
 }
 </script>
