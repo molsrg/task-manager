9573b225aab53e2f0373f27e96c3476f4b101677
Timealiver <timealive@mail.ru>
2023-08-11 17:02:58 +0300

    Registration function made

--- backend/authController.js
+++ backend/authController.js
@@ -1,25 +1,40 @@
 const User = require('./models/User');
 const Role = require('./models/Role');
-
+const bcrypt = require('bcryptjs');
 class authController {
   async registration(req, res) {
     try {
+      const { username, email, password } = req.body;
+      const candidateUsername = await User.findOne({ username });
+      const candidateEmail = await User.findOne({});
+      if (candidateUsername) {
+        res
+          .status(400)
+          .json({ message: 'Пользователь с таким именем уже существует' });
+      }
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const userRole = await Role.findOne({ value: 'USER' });
+      const user = new User({
+        username,
+        password: hashPassword,
+        roles: [userRole.value],
+      });
+      await user.save();
+      return res.status(200).json('Пользователь создан');
     } catch (e) {
       console.log(e);
+      res.status(400).json({ message: 'Registration failed' });
     }
   }
   async login(req, res) {
     try {
     } catch (e) {
       console.log(e);
+      res.status(400).json({ message: 'Login failed' });
     }
   }
   async getUsers(req, res) {
     try {
-      const userRole = new Role();
-      const adminRole = new Role({ value: 'ADMIN' });
-      await userRole.save();
-      await adminRole.save();
       res.json('Getting users');
     } catch (e) {
       console.log(e);
