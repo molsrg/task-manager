00f9d488e1ab9152fb87f78cab5be87b1ce20c52
Timealiver <timealive@mail.ru>
2023-08-11 16:17:41 +0300

    added Schema for User

--- backend/authController.js
+++ backend/authController.js
@@ -1,5 +1,29 @@
+const User = require('./models/User');
+const Role = require('./models/Role');
+
 class authController {
-  async registration() {}
-  async login() {}
-  async getUsers() {}
+  async registration(req, res) {
+    try {
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  async login(req, res) {
+    try {
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  async getUsers(req, res) {
+    try {
+      const userRole = new Role();
+      const adminRole = new Role({ value: 'ADMIN' });
+      await userRole.save();
+      await adminRole.save();
+      res.json('Getting users');
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
+module.exports = new authController();
--- backend/authRouter.js
+++ backend/authRouter.js
@@ -1,7 +1,8 @@
 const Router = require('express');
 const router = new Router();
-router.post('/registration');
-router.post('/login');
-router.get('/users');
+const controller = require('./authController');
+router.post('/registration', controller.registration);
+router.post('/login', controller.login);
+router.get('/users', controller.getUsers);
 
 module.exports = router;
