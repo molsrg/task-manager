07233dd8f650b54cf0f16571c2727073ec71e901
Timealiver <timealive@mail.ru>
2023-08-11 21:35:01 +0300

    Created login system with JWT

--- backend/authController.js
+++ backend/authController.js
@@ -2,6 +2,15 @@ const User = require('./models/User');
 const Role = require('./models/Role');
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const { secret } = require('./config');
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  };
+  return jwt.sign(payload, secret, { expiresIn: '24h' });
+};
 class authController {
   async registration(req, res) {
     try {
@@ -40,6 +49,17 @@ class authController {
   }
   async login(req, res) {
     try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+        res.status(400).json({ message: `Пользователь ${username} не найден` });
+      }
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) {
+        res.status(400).json({ message: `Введен неверный пароль` });
+      }
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({ token });
     } catch (e) {
       console.log(e);
       res.status(400).json({ message: 'Login failed' });
