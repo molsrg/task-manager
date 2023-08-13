8444bd1f3ad64c9aec45c519e044c75e9eec9dc4
timealiver <timealive@mail.ru>
2023-08-13 18:58:58 +0300

    Edited merge problems

--- client/src/components/Auth/AuthorizationClient.vue
+++ client/src/components/Auth/AuthorizationClient.vue
@@ -11,19 +11,11 @@
       <div class="section__auth">
         <h3 class="section__title">{{ title }}</h3>
         <button @click="AuthOrReg" class="section__subtitle">{{ subtitle }}</button>
-        <h3 class="section__title">{{ title }}</h3>
-        <button @click="AuthOrReg" class="section__subtitle">{{ subtitle }}</button>
       </div>
       
     </div>
     <div class="section__item">
       <form action="" method="post">
-        <input v-if="this.title == 'Регистрация' " class="section__input" type="text" placeholder="Имя пользователя" v-model="login">
-
-        <input class="section__input" type="email" placeholder="E-mail" v-model="mail">
-        <input class="section__input" type="password" placeholder="Пароль" v-model="password">
-        <input v-if="this.title == 'Регистрация'" class="section__input" type="password" placeholder="Подтверждение пароля" v-model="confirm_password">
-        <a v-if="this.title == 'Вход'" class="section__forget" href="#">Забыли пароль?</a>
         <input v-if="this.title == 'Регистрация' " class="section__input" type="text" placeholder="Имя пользователя" v-model="login">
 
         <input class="section__input" type="email" placeholder="E-mail" v-model="mail">
@@ -40,12 +32,6 @@
         <span>или</span>
       </div>
 
-      
-      <button type="submit" class="section__button" @click="authUser">{{ auth }}</button>
-      <div class="section__text-or">
-        <span>или</span>
-      </div>
-
       <div class="section__bt-container">
         <a href="#"><img src="../../assets/images/auth/google.svg"></a>
         <a href="#"><img src="../../assets/images/auth/github.svg"></a>
@@ -53,7 +39,6 @@
       </div>
 
     </div>
-</div>
 </div>
   <!-- <img :src="flag == true ? require('../../assets/images/auth/ile.png') : require('../../assets/images/auth/ile2.png')"> -->
 
@@ -73,15 +58,6 @@ export default {
       title: 'Регистрация', 
       subtitle: 'Вход', 
       auth: 'Зарегистрироваться'
-      login: '', 
-      mail: '', 
-      password: '', 
-      confirm_password: '', 
-
-      title: 'Регистрация', 
-      subtitle: 'Вход', 
-      auth: 'Зарегистрироваться'
-
     }
   }, 
   methods: {
