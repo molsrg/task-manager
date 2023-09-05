688b5e27f153983c7c77dc4a81e732fcb6a73167
mlsrg <molsrg.35@gmail.com>
2023-09-05 15:02:23 +0300

    Added functionality to main
    Made a display in an array from the registration date to the current date. I made constant tracking of the current day and adjusted the color to it. I laid out the basic layout of the calendar.

--- client/src/components/Auth/AuthorizationFromService.vue
+++ client/src/components/Auth/AuthorizationFromService.vue
@@ -1,70 +1,60 @@
 <template>
-  <div class="wrapper">
+  <!-- <div class="wrapper">
     <span class="loader"></span>
-  </div>
+  </div> -->
 
-  <div class="select">
-    <div class="select__header">
-      <div class="select__current">{{ currentMonth }}</div>
-      <img
-        @click="showCalendar = !showCalendar"
-        :class="showCalendar ? 'select__btn' : 'select__btn-isActive'"
-        src="../../assets/images/main/select__btn.svg"
-        alt=""
-      />
-    </div>
-    <div :class="showCalendar ? 'select__body-show' : 'select__body'">
-      <div
-        @click="changeCurrent(month)"
-        class="select__item"
-        v-for="month in months"
-        :key="month"
-      >
-        {{ month }}
+  <div class="select__arrow">
+    <div class="select">
+      <div class="select__header">
+        <div class="select__current">{{ currentMonth }}</div>
+        <img
+          @click="showCalendar = !showCalendar"
+          :class="showCalendar ? 'select__btn' : 'select__btn-isActive'"
+          src="../../assets/images/main/select__btn.svg"
+          alt=""
+        />
+      </div>
+      <div :class="showCalendar ? 'select__body-show' : 'select__body'">
+        <div
+          @click="changeCurrent(month)"
+          class="select__item"
+          v-for="month in months"
+          :key="month"
+        >
+          {{ month }}
+        </div>
       </div>
     </div>
-  </div>
-
-  <div class="arrows">
-    <img src="../../assets/images/main/left-arrow.svg" alt="" class="arrow" />
-    <img src="../../assets/images/main/right-arrow.svg" alt="" class="arrow" />
+    <div class="arrows">
+      <img
+        @click="prevWeek()"
+        src="../../assets/images/main/left-arrow.svg"
+        alt=""
+        class="arrows__item"
+      />
+      <img
+        @click="nextWeek()"
+        src="../../assets/images/main/right-arrow.svg"
+        alt=""
+        class="arrows__item"
+      />
+    </div>
   </div>
 
   <div class="calendar">
     <div class="week">
-      <div class="day">
-        <div class="day__number">02</div>
-        <div class="day__text">пн</div>
-        <div class="day__line">―</div>
-      </div>
-      <div class="day">
-        <div class="day__number">03</div>
-        <div class="day__text">вт</div>
-        <div class="day__line">―</div>
-      </div>
-      <div class="day">
-        <div class="day__number">04</div>
-        <div class="day__text">ср</div>
-        <div class="day__line">―</div>
-      </div>
-      <div class="day">
-        <div class="day__number">05</div>
-        <div class="day__text">чт</div>
-        <div class="day__line">―</div>
-      </div>
-      <div class="day">
-        <div class="day__number">06</div>
-        <div class="day__text">пт</div>
-        <div class="day__line">―</div>
-      </div>
-      <div class="day">
-        <div class="day__number">07</div>
-        <div class="day__text">сб</div>
-        <div class="day__line">―</div>
-      </div>
-      <div class="day">
-        <div class="day__number">08</div>
-        <div class="day__text">вс</div>
+      <div
+        :class="
+          day[1] == presentDay[0] && day[2] == presentDay[1]
+            ? 'present-day'
+            : 'day'
+        "
+        v-for="day in days"
+        :key="day"
+      >
+        <div class="day__number">{{ day[1] }}</div>
+        <div class="day__text">{{ day[0] }}</div>
+        <!-- <div class="day__text">{{ day[2] }}</div> -->
         <div class="day__line">―</div>
       </div>
     </div>
@@ -179,41 +169,111 @@
 
 <script>
 import axios from "axios";
+import moment from "moment";
 
 export default {
   data() {
     return {
-      showCalendar: false,
+      presentDay: "",
       currentMonth: "",
-      months: [
-        "January 2023",
-        "February 2023",
-        "March 2023",
-        "April 2023",
-        "May 2023",
-        "June 2023",
-        "July 2023",
-        "August 2023",
-        "September 2023",
-        "October 2023",
-        "November 2023",
-        "December 2023",
-      ],
+
+      days: [],
+      months: [],
+
+      showCalendar: false,
+
+      registrationMonth: "01-04-2023", // user
     };
   },
   mounted() {
-    this.OAuth();
-    this.getMonth();
+    // this.OAuth();
+    this.momentjs();
+    this.getMonths(this.registrationMonth); // загрузка в  select всех месяцев с даты регистрации по текущий
   },
 
   methods: {
+    momentjs() {
+      const currentDate = moment();
+      currentDate.locale("ru");
+      this.currentMonth = this.capitalizeFirstLetter(
+        currentDate.format("MMMM YYYY")
+      ); // установка текущего месяца
+      this.presentDay = currentDate.format("DD MMMM").split(" "); // установка текущего дня
+
+      const weekStart = currentDate.clone().startOf("week");
+      for (let i = 0; i <= 6; i++) {
+        const day = moment(weekStart).add(i, "days").format("dddd DD MMMM");
+        this.days.push(day.split(" "));
+      }
+    },
+
+    getMonths(startDate, monthAfter = 3) {
+      const nowNormalized = moment().locale("ru").startOf("month"); // Первое число текущего месяца
+      const startDateNormalized = moment(startDate, "DD-MM-YYYY")
+        .locale("ru")
+        .startOf("month");
+
+      while (startDateNormalized.isBefore(nowNormalized)) {
+        this.months.push(
+          this.capitalizeFirstLetter(startDateNormalized.format("MMMM YYYY"))
+        );
+        startDateNormalized.add(1, "M");
+      }
+      // Добавляем текущий месяц
+      // this.months.push(nowNormalized.format("MMMM YYYY"));
+
+      // Добавляем месяцы после текущего
+      for (let i = 0; i < monthAfter; i++) {
+        const monthslater = nowNormalized.clone().add(i, "M");
+        this.months.push(
+          this.capitalizeFirstLetter(monthslater.format("MMMM YYYY"))
+        );
+      }
+    },
+
     changeCurrent(value) {
       this.currentMonth = value;
     },
-    getMonth() {
-      const today = new Date();
-      const currentMonth = this.months[today.getMonth()];
-      this.currentMonth = currentMonth;
+    prevWeek() {
+      if (!this.currentWeek) {
+        this.currentWeek = moment()
+          .startOf("isoWeek")
+          .subtract(1, "week")
+          .locale("ru");
+      } else {
+        this.currentWeek.subtract(1, "week").locale("ru");
+      }
+
+      this.days = [];
+
+      for (let i = 0; i <= 6; i++) {
+        const day = moment(this.currentWeek)
+          .add(i, "days")
+          .format("dddd DD MMMM");
+        this.days.push(day.split(" "));
+      }
+    },
+    nextWeek() {
+      if (!this.currentWeek) {
+        // Если текущая неделя не определена, создаем ее и устанавливаем в текущую неделю
+        this.currentWeek = moment().add(1, "week").startOf("isoWeek");
+      } else {
+        // Иначе переключаемся на следующую неделю
+        this.currentWeek.add(1, "week");
+      }
+
+      this.currentWeek.locale("ru");
+      this.days = [];
+
+      for (let i = 0; i <= 6; i++) {
+        const day = moment(this.currentWeek)
+          .add(i, "days")
+          .format("dddd DD MMMM");
+        this.days.push(day.split(" "));
+      }
+    },
+    capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
     },
 
     // 1) здесь я сделал минимальную разметку
