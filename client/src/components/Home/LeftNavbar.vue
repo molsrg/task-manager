88ba0daa2c320d8a9650c5e99586408c57fb7f8e
mlsrg <molsrg.35@gmail.com>
2023-09-09 20:55:22 +0300

    Added changes

---
+++ client/src/components/Home/LeftNavbar.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="leftnavbar">
+        <div class="people-container">
+          <img class="logo people-container__logo" src="../../assets/images/auth/logo.svg" alt="logo">
+
+          <div class="people-container__user-container user-container">
+
+            <h3 class="user-container__user-count">3</h3>
+            <img src="../../assets/images/home/line__horizont.svg" alt="">
+            <img src="../../assets/images/home/zaxar.png" alt="" class="user-container__user">
+            <img src="../../assets/images/home/zaxar.png" alt="" class="user-container__user">
+            <img src="../../assets/images/home/zaxar.png" alt="" class="user-container__user">
+            <button class="user-container__add-user"><img src="../../assets/images/home/addpeople.svg" alt=""></button>
+            
+          
+          </div>
+          <div></div>
+    </div>
+
+
+    <div class="task-container">
+        <h3 class="logo__text task-container__logo-text">Task Unity Tech</h3>
+        <div class="task-container__tasklist tasklist">
+            <div class="tasklist__title">
+                <h4 class="tasklist__text">Эта неделя </h4>
+                <img class="tasklist__show" src="../../assets/images/home/minus.svg" alt="">
+            </div>
+            <div class="tasklist__tasks">
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Утренняя рутина</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked>Что-то общее</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Обеденный перерыв</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Google Inc.</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Google Inc.</label>
+            </div> 
+        </div>
+        <div class="task-container__tasklist tasklist">
+            <div class="tasklist__title">
+                <h4 class="tasklist__text">Эта неделя </h4>
+                <img class="tasklist__show" src="../../assets/images/home/minus.svg" alt="">
+            </div>
+            <div class="tasklist__tasks">
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Утренняя рутина</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked>Что-то общее</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Обеденный перерыв</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Google Inc.</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Google Inc.</label>
+            </div> 
+            
+        </div>
+        <div class="task-container__tasklist tasklist">
+            <div class="tasklist__title">
+                <h4 class="tasklist__text">Эта неделя </h4>
+                <img class="tasklist__show" src="../../assets/images/home/minus.svg" alt="">
+            </div>
+            <div class="tasklist__tasks">
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Утренняя рутина</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked>Что-то общее</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Обеденный перерыв</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Google Inc.</label>
+                <label><input class="tasklist__name" type="checkbox" name="brands" value="1" checked> Google Inc.</label>
+            </div> 
+            
+        </div>
+    </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
